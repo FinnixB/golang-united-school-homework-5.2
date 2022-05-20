3361311f32bb4f25d135d452d3e5031ee736ab8d
Pavel Demkovich <finn@finnix.servebeer.com>
2022-05-20 22:34:55 +0300

    final solution

--- cache.go
+++ cache.go
@@ -1,23 +1,59 @@
 package cache
 
-import "time"
+import (
+	"time"
+)
+
+type cacheValue struct {
+	value string
+	valid time.Time
+}
 
 type Cache struct {
+	cache     map[string]cacheValue
+	timelimit int
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{cache: make(map[string]cacheValue), timelimit: 300}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (this *Cache) Valid(key string) bool {
+	cvalue, exists := this.cache[key]
 
+	if exists && cvalue.valid.Sub(time.Now()).Seconds() > 0.0 {
+		return true
+	}
+
+	return false
 }
 
-func (receiver) Put(key, value string) {
+func (this *Cache) Get(key string) (string, bool) {
+	cvalue, exists := this.cache[key]
+
+	if exists && cvalue.valid.Sub(time.Now()).Seconds() > 0.0 {
+		return cvalue.value, true
+	}
+
+	return "", false
 }
 
-func (receiver) Keys() []string {
+func (this *Cache) Put(key, value string) {
+	this.cache[key] = cacheValue{value: value, valid: time.Now().Add(time.Second * time.Duration(this.timelimit))}
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (this *Cache) Keys() []string {
+	keys := make([]string, 0, len(this.cache))
+
+	for key := range this.cache {
+		if this.Valid(key) {
+			keys = append(keys, key)
+		}
+	}
+
+	return keys
+}
+
+func (this *Cache) PutTill(key, value string, deadline time.Time) {
+	this.cache[key] = cacheValue{value: value, valid: deadline}
 }
